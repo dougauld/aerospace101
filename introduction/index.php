4c2905fc3be9ef6971f723ccc8350a22b1f27328
Doug Auld <douglass.auld@gmail.com>
2025-10-29 16:42:11 +1100

    Add files via upload

---
+++ introduction/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<?php
+include "../aero4-header.php";
+?>
+
+<h3><span style="color: #800000;">INTRODUCTION</span></h3>
+<h2><font color="#800000">What is Aerodynamics?</font></h2>
+<div><img src="airtruk.png" alt="airtruk" width="599" height="302" align="right" />
+Aerodynamics is the branch of dynamics that treats the motion of air 
+(and other gaseous fluids) and the resulting forces acting on solid objects 
+moving about within such a fluid.</div>
+<p>Aerodynamic results will fall into different categories of behaviour depending on velocity range (slow speed, high speed, supersonic,hypersonic), depending on size and shape of the object (large, small, complex 3D solid) and the physical properties of the fluid (dense, rarefied, viscous, inviscid). Many different aerodynamic situations can be analysed using a range of available theories.</p>
+<p>The important steps of</p>
+<ul>
+<li>flow field definition,</li>
+<li>calculation of velocity field around the object,</li>
+<li>calculation of flow pressure and shear distribution,</li>
+<li>integration of these distributions on the surface of the body</li>
+</ul>
+<p>are the tools used for most theoretical aerodynamic prediction. The aim is to be able to predict the lift, drag, thrust and moments acting on objects or vehicles in motion.</p>
+<h2><font color="#800000">What is Lift?</font></h2>
+</span></b></span></p>
+<p>Lift is the aerodynamic force acting at right angles to the direction of motion of the object. It is produced by the interaction of the moving object and the fluid. This interaction typically leads to a pressure difference being created between upper and lower surfaces of the object. The net effect is that a high pressure is produced below the object and low pressure above the object. These pressures produce a force which sustains the object against descent due to gravity. The physical mechanisms involved in the fluid/body interaction that creates lift are very complex. The laws of conservation of mass and momentum, including the effect of fluid rotation, cause complex fluid flow paths around the object as well as velocity and pressure distributions which produce the resultant lift. These flow features can significantly change due to small changes in flow angle or surface curvature. It is hoped that by studying the following chapters on the theoretical basis of fluid flow, students will begin to understand the details behind these flow mechanisms</p>
+<p><a href="images-of-lift.php">Here are some diagrams showing the creation of lift</a>.</p>
+<h2><font color="#800000">What is Drag?</font></h2>
+<p>Drag is the aerodynamic force resisting the motion of the object through the fluid. It is produced by front/rear pressure differences, shearing between fluid and solid surface
+compression of the gas at high speed and residual lift components induced by three-dimensional flow rotation.</p>
+<h2><font color="#800000">What is Thrust?</font></h2>
+<p>Thrust is the aerodynamic force produced in the direction of motion and is required to overcome drag and thus sustain the forward flight of the vehicle. It is produced by mechanical
+means (an engine) which effectively transfers energy into the flow, in the form of increased fluid momentum. Thrust is the forward reaction to this fluid momentum change.</p>
+<h2><font color="#800000">What is Moment?</font></h2>
+<p>Moment is the aerodynamic torque produced by out of balance forces. An object or vehicle has no solid structure to support it in the air. A balance is required and all forces must act through the same point, typically the center of gravity of the object. Any variation of the point of application for aerodynamic forces will produce a couple, leading to a moment which will cause the vehicle to start to rotate. The study of these moments and the effect they have on the stability of motion of the vehicle is covered in more detail in the Flight Mechanics courses.</p>
+<h2><font color="#800000">What is Contained in these Pages?</font></h2>
+<p>In the chapters of this web text we try to explain methods of analysis that can be used to predict the behaviour of various flight vehicles and components. The aim is to provide
+introductory engineering tools that will help in the understanding of fundamental aerodynamics.</p>
+<p>The SI system of units is used for data, formula and calculations in this text.</p>
+
+<?php
+include "../aero4-footer.php";
+?>
