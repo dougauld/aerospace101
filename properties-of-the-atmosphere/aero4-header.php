8f5992d48098c99179b4ef1ca218b21ee5ab4678
Doug Auld <douglass.auld@gmail.com>
2025-10-31 09:38:51 +1100

    Add files via upload

---
+++ properties-of-the-atmosphere/aero4-header.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=10" />
+<title>Resources | Aerodynamics for Students</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="/xmlrpc.php" />
+<script type="text/javascript">
+ window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.3.1"}};
+!function(a,b,c){function d(a){var c=b.createElement("canvas"),d=c.getContext&&c.getContext("2d");return d&&d.fillText?(d.textBaseline="top",d.font="600 32px Arial","flag"===a?(d.fillText(String.fromCharCode(55356,56812,55356,56807),0,0),c.toDataURL().length>3e3):(d.fillText(String.fromCharCode(55357,56835),0,0),0!==d.getImageData(16,16,1,1).data[0])):!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g;c.supports={simple:d("simple"),flag:d("flag")},c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.simple&&c.supports.flag||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+</script>
+<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='wp-quicklatex-format-css'  href='/wp-content/plugins/wp-quicklatex/css/quicklatex-format.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='siteorigin-panels-front-css'  href='/wp-content/plugins/siteorigin-panels/css/front.css?ver=2.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='vantage-style-css'  href='/wp-content/themes/vantage/style.css?ver=1.4.4' type='text/css' media='all' />
+<link rel='stylesheet' id='font-awesome-css'  href='/wp-content/themes/vantage/fontawesome/css/font-awesome.css?ver=4.2.0' type='text/css' media='all' />
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='/wp-content/themes/vantage/js/jquery.flexslider.min.js?ver=2.1'></script>
+<script type='text/javascript' src='/wp-content/themes/vantage/js/jquery.touchSwipe.min.js?ver=1.6.6'></script>
+<script type='text/javascript' src='/wp-content/themes/vantage/js/jquery.theme-main.min.js?ver=1.4.4'></script>
+<script type='text/javascript' src='/wp-content/themes/vantage/js/jquery.fitvids.min.js?ver=1.0'></script>
+<script type='text/javascript' src='/wp-content/plugins/wp-quicklatex/js/wp-quicklatex-frontend.js?ver=1.0'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="/wp-includes/wlwmanifest.xml" /> 
+<link rel='canonical' href='/index.php/resources/' />
+<link rel='shortlink' href='/?p=364' />
+	<!--[if lt IE 9]>
+		<script src="/wp-content/themes/vantage/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+	<!--[if (gte IE 6)&(lte IE 8)]>
+		<script type="text/javascript" src="/wp-content/themes/vantage/js/selectivizr.js"></script>
+	<![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1" />	
+<style type="text/css" media="screen">
+	#footer-widgets .widget { width: 100%; }
+	@media screen and (max-width: 640px) {
+  	 #footer-widgets .widget { width: auto; float: none; }
+	}
+</style>
+<link rel="icon" href="/wp-content/uploads/2015/11/cropped-aero4students-32x32.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2015/11/cropped-aero4students-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2015/11/cropped-aero4students-180x180.png">
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2015/11/cropped-aero4students-270x270.png">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=UnifrakturMaguntia">
+<link rel="stylesheet" href="/jqmath/jqmath-0.4.3.css">
+<script src="/jqmath/jquery-1.4.3.min.js"></script>
+<script src="/jqmath/jqmath-etc-0.4.6.min.js" charset="utf-8"></script>
+
+</head>
+
+<body class="page page-id-364 page-parent page-template page-template-templates page-template-template-full-notitle page-template-templatestemplate-full-notitle-php siteorigin-panels responsive layout-full no-js has-menu-search panels-style-force-full">
+<div id="page-wrapper">
+ <header id="masthead" class="site-header masthead-logo-in-menu" role="banner">
+ <nav role="navigation" class="site-navigation main-navigation primary use-sticky-menu">
+ <div class="full-container">
+  <a href="/" title="Aerodynamics for Students" rel="home" class="logo"><img src="/wp-content/uploads/2015/11/aero4logo2.png"  class="logo-height-constrain"  width="1000"  height="100"  alt="Aerodynamics for Students Logo"  /></a>
+  <div id="search-icon">
+   <div id="search-icon-icon">
+    <div class="vantage-icon-search">
+    </div>
+   </div>
+   <form method="get" class="searchform" action="/" role="search">
+   <input type="text" class="field" name="s" value="" placeholder="Search"/>
+   </form>
+  </div>
+  <div class="menu-menu-1-container"><ul id="menu-menu-1" class="menu"><li id="menu-item-249" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-249"><a href="/">Welcome</a></li>
+   <li id="menu-item-222" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-222"><a href="/toc.php">Table of Contents</a>
+   <ul class="sub-menu">
+   <li id="menu-item-223" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-223"><a href="/introduction/">Introduction</a></li>
+   <li id="menu-item-224" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-224"><a href="/fluid-mechanics/">Fluid Mechanics</a></li>
+   <li id="menu-item-225" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-225"><a href="/flow-visualisation/">Flow Visualisation</a></li>
+   <li id="menu-item-226" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-226"><a href="/properties-of-the-atmosphere/">Properties of the Atmosphere</a>
+   <ul class="sub-menu">
+   <li id="menu-item-227" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-227"><a href="/properties-of-the-atmosphere/sea-level-conditions.php">Sea Level Conditions</a></li>
+   <li id="menu-item-228" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-228"><a href="/properties-of-the-atmosphere/variation-with-altitude.php">Variation with Altitude</a></li>
+   <li id="menu-item-229" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-229"><a href="/properties-of-the-atmosphere/variation-due-to-local-ground-conditions.php">Variation due to Local Ground Conditions</a></li>
+   <li id="menu-item-230" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-230"><a href="/properties-of-the-atmosphere/variation-due-to-humidity.php">Variation due to Humidity</a></li>
+   <li id="menu-item-231" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-231"><a href="/properties-of-the-atmosphere/variation-due-to-extremely-altitude.php">Variation Due to Extremely Altitude</a></li>
+   <li id="menu-item-232" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-232"><a href="/properties-of-the-atmosphere/table-of-atmospheric-properties.php">Table of Atmospheric Properties</a></li>
+   </ul>
+   </li>
+   <li id="menu-item-233" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-233"><a href="/subsonic-aerofoil-and-wing-theory/">Subsonic Aerofoil and Wing Theory</a>
+   <ul class="sub-menu">
+   <li id="menu-item-234" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-234"><a href="/subsonic-aerofoil-and-wing-theory/aerofoil-section-2-d-geometry.php">Aerofoil Section 2-D Geometry</a></li>
+   <li id="menu-item-235" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-235"><a href="/subsonic-aerofoil-and-wing-theory/joukowski-aerofoils-and-flow-mapping.php">Joukowski Aerofoils and Flow Mapping</a></li>
+   <li id="menu-item-236" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-236"><a href="/subsonic-aerofoil-and-wing-theory/2-d-thin-aerofoil-theory.php">2-D Thin Aerofoil Theory</a></li>
+   <li id="menu-item-237" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-237"><a href="/subsonic-aerofoil-and-wing-theory/2d-panel-methods.php">2D Panel Methods</a></li>
+   <li id="menu-item-238" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-238"><a href="/subsonic-aerofoil-and-wing-theory/2d-boundary-layer-modelling.php">2D Boundary Layer Modelling</a></li>
+   <li id="menu-item-239" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-239"><a href="/subsonic-aerofoil-and-wing-theory/3d-prandtl-lifting-line-theory.php">3D Prandtl Lifting Line Theory</a></li>
+   <li id="menu-item-240" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-240"><a href="/subsonic-aerofoil-and-wing-theory/3d-vortex-lattice-method.php">3D Vortex Lattice Method</a></li>
+   <li id="menu-item-241" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-241"><a href="/subsonic-aerofoil-and-wing-theory/subsonic-compressibility-corrections.php">Subsonic Compressibility Corrections</a></li>
+   </ul>
+   </li>
+   <li id="menu-item-242" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-242"><a href="/gas-dynamics-and-supersonic-flow/">Gas Dynamics and Supersonic Flow</a></li>
+   <li id="menu-item-243" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-243"><a href="/propulsion/">Propulsion</a>
+   <ul class="sub-menu">
+   <li id="menu-item-244" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-244"><a href="/propulsion/blade-element-propeller-theory.php">Blade Element Propeller Theory</a></li>
+   <li id="menu-item-245" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-245"><a href="/propulsion/blade-element-rotor-theory.php">Blade Element Rotor Theory</a></li>
+   <li id="menu-item-246" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-246"><a href="/propulsion/gas-turbine-analysis.php">Gas Turbine Analysis</a></li>
+   <li id="menu-item-247" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-247"><a href="/propulsion/simple-rocket-analysis.php">Simple Rocket Analysis</a></li>
+   </ul>
+   </li>
+   <li id="menu-item-248" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-248"><a href="/aircraft-instruments/">Aircraft Instruments</a></li>
+   <li id="menu-item-275" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-275"><a href="/aircraft-performance/">Aircraft Performance</a></li>
+   <li id="menu-item-356" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-356"><a href="/aeroelasticity/">Aeroelasticity</a></li>
+   <li id="menu-item-363" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-363"><a href="/wind-tunnel-simulation/">Wind Tunnel Simulation</a>
+   <ul class="sub-menu">
+   <li id="menu-item-416" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-416"><a href="/wind-tunnel-simulation/admin-office.php">Admin Office</a></li>
+   <li id="menu-item-520" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-520"><a href="/wind-tunnel-simulation/pattern-shop.php">Pattern Shop</a></li>
+   <li id="menu-item-521" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-521"><a href="/wind-tunnel-simulation/wind-tunnel-laboratory.php">Wind Tunnel Laboratory</a></li>
+   </ul>
+   </li>
+   </ul>
+   </li>
+   <li id="menu-item-366" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-364 current_page_item menu-item-366"><a href="/resources/">Resources</a></li>
+   <li id="menu-item-535" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-535"><a href="/contact-us/">Contact Us</a></li>
+   </ul>
+  </div>
+ </div>
+ </nav><!-- .site-navigation .main-navigation -->
+ </header><!-- #masthead .site-header -->
+  <div id="main" class="site-main"> 
+  <div class="full-container">
+   <div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+     <article id="post-364" class="post-364 page type-page status-publish hentry post">
+     <div class="entry-main">
+     <div class="entry-content"> 
